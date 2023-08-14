10a40523178b1a7f25ed5543bbce1a856070d9d4
valerii.korniienko <vakor.dev@gmail.com>
2023-08-14 18:02:11 +0300

    Basic ShopOptions UI

--- Assets/_Scripts/Repositories/Shop.cs
+++ Assets/_Scripts/Repositories/Shop.cs
@@ -1,12 +1,42 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 using _Scripts.Helpers;
 using _Scripts.ScriptableObjects;
+using _Scripts.UI;
 using UnityEngine;
 
 namespace _Scripts.Repositories
 {
     public class Shop : StaticInstance<Shop>
     {
+        private ShopOption[] _availableOptions;
+        public event Action OnShopOptionChanged;
+        public ShopOption CurrentShopOption { get; private set; }
+        private int _currentShopOptionIndex;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _availableOptions = new[]
+            {
+                new ShopOption("x1", ShopOptionType.Additive, 1),
+                new ShopOption("10%", ShopOptionType.Percent, 10),
+                new ShopOption("50%", ShopOptionType.Percent, 50),
+                new ShopOption("MAX", ShopOptionType.Percent, 100),
+            };
+
+            _currentShopOptionIndex = 0;
+            CurrentShopOption = _availableOptions[_currentShopOptionIndex];
+        }
+
+        public void ChooseNextShopOption()
+        {
+            _currentShopOptionIndex = (_currentShopOptionIndex + 1) % _availableOptions.Length;
+            CurrentShopOption = _availableOptions[_currentShopOptionIndex];
+            OnShopOptionChanged?.Invoke();
+        }
+
         public bool TryBuyResource(ResourceSO resourceSO, int quantity)
         {
             if (MarketRepository.Instance.TryGetMarketItem(resourceSO, out var marketItemSO))
---
+++ Assets/_Scripts/UI/ChooseBuyQuantityUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using _Scripts.Repositories;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.UI
+{
+    public class ChooseBuyQuantityUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI buyButtonText;
+        [SerializeField] private Button buyButton;
+
+        private void Start()
+        {
+            buyButton.onClick.AddListener(()=>Shop.Instance.ChooseNextShopOption());
+            Shop.Instance.OnShopOptionChanged += SetBuyButtonText;
+            
+            SetBuyButtonText();
+        }
+
+        private void SetBuyButtonText()
+        {
+            buyButtonText.text = Shop.Instance.CurrentShopOption.DisplayText;
+        }
+    }
+
+    public struct ShopOption
+    {
+        public string DisplayText { get; private set; }
+        public int Value { get; private set; }
+        public ShopOptionType Type { get; private set; }
+
+        public ShopOption(string displayText, ShopOptionType type, int value)
+        {
+            DisplayText = displayText;
+            Value = value;
+            Type = type;
+        }
+    }
+
+    public enum ShopOptionType
+    {
+        Additive,
+        Percent
+    }
+}
